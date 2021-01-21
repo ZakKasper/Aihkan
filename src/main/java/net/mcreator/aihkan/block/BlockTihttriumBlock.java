22e58606a7285401538c259bd9ad0f3be9429ba7
tpk73 <tpaaka73@gmail.com>
2021-01-20 19:52:22 -0600

    new block Tihttrium - still needs worked on

---
+++ src/main/java/net/mcreator/aihkan/block/BlockTihttriumBlock.java
@@ -0,0 +1,48 @@
+
+package net.mcreator.aihkan.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.aihkan.AihkanModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AihkanModElements.ModElement.Tag
+public class BlockTihttriumBlock extends AihkanModElements.ModElement {
+	@ObjectHolder("aihkan:block_tihttrium")
+	public static final Block block = null;
+	public BlockTihttriumBlock(AihkanModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("block_tihttrium");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
